3c0eeecd9a8b4f9fce9d7660c80b55e6b1972544
Vytautas Pleckaitis <vytautas@email.com>
2022-09-27 15:27:51 +0100

    use hsts

--- Startup.cs
+++ Startup.cs
@@ -57,7 +57,7 @@ namespace WebApp
             //{
             //    app.UseExceptionHandler("/Error");
             //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //    app.UseHsts();
+                app.UseHsts();
             //}
 
             app.UseSwagger();
